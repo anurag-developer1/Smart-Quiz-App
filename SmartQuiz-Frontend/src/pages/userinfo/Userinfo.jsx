9895524542934647d9342f4c061cb5e231b210ca
Anurag <anurag18994@gmail.com>
2024-06-16 11:11:32 +0530

    User info bug removed

--- SmartQuiz-Frontend/src/pages/userinfo/Userinfo.jsx
+++ SmartQuiz-Frontend/src/pages/userinfo/Userinfo.jsx
@@ -1,18 +1,21 @@
 import { useSelector, useDispatch } from "react-redux";
 import { setusername } from "../../reduxstateslices/playQuizSlice";
 import { useState } from "react";
-import { NavLink } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 //this component saves the name of the player before starting the quiz. the players name must be 5 to 50 characters
 function Userinfo() {
+  const navigate=useNavigate();
   const { currentquiz } = useSelector((store) => store.playQuiz);
   const currentquiztitle = currentquiz.quiztitle;
   const currentquizdescription = currentquiz.quizdescription;
   const [playerName, setPlayerName] = useState("");
   const dispatch = useDispatch();
+  const handlePlayQuiz=()=>{
+    if((playerName.length>=5&&playerName.length<=50)){dispatch(setusername(playerName)); navigate('/displayquestions')} 
+    else return
+  }
 
-  const setPlayerUsername = () => {
-    dispatch(setusername(playerName));
-  };
+  
 
   return (
     <div className="flex items-center justify-center min-h-screen">
@@ -31,19 +34,17 @@ function Userinfo() {
             className="w-full px-4 py-2 rounded-md border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500"
           />
         </div>
-        <NavLink
-          to="/displayquestions"
-          className="block w-full text-center"
-          onClick={setPlayerUsername}
-        >
+        
+        
           <button className={`${
                   playerName.length>=5&&playerName.length<=50?"bg-blue-500 hover:bg-blue-600 text-white"
                     : "bg-gray-300 text-gray-600 cursor-not-allowed"
                     
-                } font-bold py-2 px-4 mt-4 rounded transition-colors duration-200 w-full`}>
+                } font-bold py-2 px-4 mt-4 rounded transition-colors duration-200 w-full`}
+                onClick={handlePlayQuiz}>
             Play Quiz
           </button>
-        </NavLink>
+        
       </div>
     </div>
   );
